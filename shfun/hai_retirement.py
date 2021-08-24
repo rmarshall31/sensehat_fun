9e913fd306d4e91f631abb53d1c7083a2b8c5484
Russ Marshall <rmarshall@synamedia.com>
2021-08-24 18:43:58 -0400

    whoops

--- shfun/hai_retirement.py
+++ shfun/hai_retirement.py
@@ -18,7 +18,6 @@ class General:
         weeks_left = int((total_days_left.days - (years_left * 360)) / 7)
         days_left = int(total_days_left.days - (years_left * 360) - (weeks_left * 7))
         message = f'{years_left} years, {weeks_left} weeks, {days_left} days until Hai retires'
-        print(message)
         self.sense.show_message(message, text_colour=[150, 150, 150], scroll_speed=0.1)
         self.sense.clear()
 
